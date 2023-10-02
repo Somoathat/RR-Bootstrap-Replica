6a3cdfdcdeaf4317c34bcbba5a59e31f0b04c4ae
Evan Graham <evangraham19@gmail.com>
2023-10-02 14:43:55 -0700

    navbar 2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
-
+import TopBar from './components/TopBar';
+import BasicExample from './components/BasicExample';
+import Background from './components/Background';
+import Storlistings from './components/Storelistings'
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <BasicExample/>
+      <Background/>
     </div>
   );
 }
